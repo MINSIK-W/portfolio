8cac80f56eea076c8ab446bc6a5876d012e932bd
minsik <minsik90@icloud.com>
2025-03-06 01:28:25 +0900

    테마 훅 추가가

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import { ThemeContext } from '@/components/Theme';
+
+// ThemeProvider 컨텍스트를 쉽게 사용하기 위한 커스텀 훅
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('"useTheme"는 반드시 "ThemeProvider" 내에서 사용💥');
+  }
+  return context;
+}
