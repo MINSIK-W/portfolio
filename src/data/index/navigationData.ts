07426f85cc0f3da3ae652920b0f8900ea565e950
minsik <minsik90@icloud.com>
2025-03-06 03:40:06 +0900

    네비게이션 데이터 생성

---
+++ src/data/index/navigationData.ts
@@ -0,0 +1,8 @@
+import { NavigationItems } from '@/types/header';
+
+export const navigationData: NavigationItems[] = [
+  { title: 'Home', path: '/' },
+  { title: 'About', path: '/about' },
+  { title: 'Project', path: '/project' },
+  { title: 'Contact', path: '/contact' },
+];
