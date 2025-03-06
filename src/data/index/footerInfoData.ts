24d3745bd21ec7ba12c230ebf3f703a32cd7fe2d
minsik <minsik90@icloud.com>
2025-03-07 04:06:45 +0900

    footer type 및 data 추가

---
+++ src/data/index/footerInfoData.ts
@@ -0,0 +1,5 @@
+import { FooterInfoItems } from '@/types/footer';
+
+export const footerInfoData: FooterInfoItems[] = [
+  { title: 'Wang Minsik ©2025 All Right Reserved.' },
+];
---
+++ src/types/footer.ts
@@ -0,0 +1,3 @@
+export interface FooterInfoItems {
+  title: string;
+}
