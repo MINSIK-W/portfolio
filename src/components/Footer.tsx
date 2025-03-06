f6c4d916fcf25045134b70f5ce8284311e0a3cc3
minsik <minsik90@icloud.com>
2025-03-07 04:07:22 +0900

    footer 컴포넌트 제작

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useTheme } from '@/hooks/useTheme';
+import Moon from './ui/icon/Moon';
+import Sun from './ui/icon/Sun';
+import { footerInfoData } from '@/data/index/footerInfoData';
+
+export default function Footer() {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <footer className="bg-bgColor dark:bg-darkBgColor shadow-theme-a dark:shadow-darkTheme-a text-textColor dark:text-darkTextColor flex flex-row items-center px-3 py-0 text-xs select-none">
+      {footerInfoData.map((item) => (
+        <p key={item.title} className="text-center">
+          {item.title}
+        </p>
+      ))}
+      <button
+        onClick={toggleTheme}
+        className="before:bg-borderColor relative cursor-pointer"
+        aria-label={theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'}
+      >
+        <i className={theme === 'dark' ? 'text-Yellow' : 'text-Red'}>
+          {theme === 'dark' ? <Moon /> : <Sun />}
+        </i>
+      </button>
+    </footer>
+  );
+}
