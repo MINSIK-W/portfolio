2d946c0466b3f3f6b2dde5590efe7df9eb0c6edb
minsik <minsik90@icloud.com>
2025-03-09 16:16:45 +0900

    경험 데이터 및 타입

---
+++ src/data/experienceData.ts
@@ -0,0 +1,9 @@
+import { experienceItems } from '@/types/experience';
+export const experienceData: experienceItems[] = [
+  {
+    title: '(주)씨엠엑스',
+    date: [{ start: '2023.10', end: '2024.09' }],
+    job: 'UX/UI팀 수석연구원 · Web Publisher · Front-end Developer · UI ENGINEER',
+    work: '하이브리드 웹 유지보수/프론트 개발/퍼블리싱',
+  },
+];
---
+++ src/types/experience.ts
@@ -0,0 +1,6 @@
+export interface experienceItems {
+  title: string;
+  date: { start: string; end: string }[];
+  job: string;
+  work: string;
+}
