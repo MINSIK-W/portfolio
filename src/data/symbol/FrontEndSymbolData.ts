2e4c2a0b04d9010b3020b12859660a1e4374f78e
minsik <minsik90@icloud.com>
2025-03-09 16:17:23 +0900

    frontEnd 데이터

---
+++ src/data/symbol/FrontEndSymbolData.ts
@@ -0,0 +1,95 @@
+import { SymbolType } from '@/types/symbol';
+import Html from '@/components/ui/symbol/Html';
+import Css from '@/components/ui/symbol/Css';
+import Javascript from '@/components/ui/symbol/Javascript';
+import Reacts from '@/components/ui/symbol/Reacts';
+import Typescript from '@/components/ui/symbol/Typescript';
+import Sass from '@/components/ui/symbol/Sass';
+import TailwindCSS from '@/components/ui/symbol/TailwindCSS';
+import Bootstrap from '@/components/ui/symbol/Bootstrap';
+import Next from '@/components/ui/symbol/Next';
+import Styledcomponents from '@/components/ui/symbol/Styledcomponents';
+import Vue from '@/components/ui/symbol/Vue';
+import Jquery from '@/components/ui/symbol/Jquery';
+import Redux from '@/components/ui/symbol/Redux';
+
+export const FrontEndSymbolData: SymbolType = {
+  html: {
+    switch: true,
+    title: 'HTML5',
+    label: 'Front-end',
+    icon: Html,
+  },
+  css: {
+    switch: true,
+    title: 'CSS3',
+    label: 'Front-end',
+    icon: Css,
+  },
+  javascript: {
+    switch: true,
+    title: 'JavaScript',
+    label: 'Front-end',
+    icon: Javascript,
+  },
+  typescript: {
+    switch: true,
+    title: 'typescript',
+    label: 'Front-end',
+    icon: Typescript,
+  },
+  react: {
+    switch: true,
+    title: 'React',
+    label: 'Front-end',
+    icon: Reacts,
+  },
+  sass: {
+    switch: true,
+    title: 'Sass',
+    label: 'Front-end',
+    icon: Sass,
+  },
+  tailwind: {
+    switch: true,
+    title: 'TailwindCSS',
+    label: 'Front-end',
+    icon: TailwindCSS,
+  },
+  bootstrap: {
+    switch: true,
+    title: 'Bootstrap',
+    label: 'Front-end',
+    icon: Bootstrap,
+  },
+  next: {
+    switch: true,
+    title: 'Next.js',
+    label: 'Front-end',
+    icon: Next,
+  },
+  styledcomponents: {
+    switch: true,
+    title: 'styled-components',
+    label: 'Front-end',
+    icon: Styledcomponents,
+  },
+  vue: {
+    switch: true,
+    title: 'Vue',
+    label: 'Front-end',
+    icon: Vue,
+  },
+  jquery: {
+    switch: true,
+    title: 'jQuery',
+    label: 'Front-end',
+    icon: Jquery,
+  },
+  redux: {
+    switch: true,
+    title: 'Redux',
+    label: 'Front-end',
+    icon: Redux,
+  },
+};
