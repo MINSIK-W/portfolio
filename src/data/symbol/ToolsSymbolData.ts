2e595a38aa09d2e09dbb96519c83093c8e5db580
minsik <minsik90@icloud.com>
2025-03-09 16:20:54 +0900

    툴 심볼 데이터

---
+++ src/data/symbol/ToolsSymbolData.ts
@@ -0,0 +1,94 @@
+import { SymbolType } from '@/types/symbol';
+import Eslint from '@/components/ui/symbol/Eslint';
+import Figma from '@/components/ui/symbol/Figma';
+import Git from '@/components/ui/symbol/Git';
+import Github from '@/components/ui/symbol/Github';
+import Illustrator from '@/components/ui/symbol/Illustrator';
+import Intellij from '@/components/ui/symbol/Intellij';
+import Mysql from '@/components/ui/symbol/Mysql';
+import Postgresql from '@/components/ui/symbol/Postgresql';
+import Mongodb from '@/components/ui/symbol/Mongodb';
+import Slack from '@/components/ui/symbol/Slack';
+import Vite from '@/components/ui/symbol/Vite';
+import Postman from '@/components/ui/symbol/Postman';
+import Npm from '@/components/ui/symbol/Npm';
+import Webpack from '@/components/ui/symbol/Webpack';
+import Yarn from '@/components/ui/symbol/Yarn';
+import Webstorm from '@/components/ui/symbol/Webstorm';
+import Photoshop from '@/components/ui/symbol/Photoshop';
+import Xd from '@/components/ui/symbol/Xd';
+
+export const ToolsSymbolData: SymbolType = {
+  eslint: {
+    switch: true,
+    title: 'ESLint',
+    label: 'Tools',
+    icon: Eslint,
+  },
+  vite: {
+    switch: true,
+    title: 'Vite',
+    label: 'Tools',
+    icon: Vite,
+  },
+  postman: {
+    switch: true,
+    title: 'Postman',
+    label: 'Tools',
+    icon: Postman,
+  },
+  npm: {
+    switch: true,
+    title: 'npm',
+    label: 'Tools',
+    icon: Npm,
+  },
+  yarn: {
+    switch: true,
+    title: 'Yarn',
+    label: 'Tools',
+    icon: Yarn,
+  },
+  webpack: {
+    switch: true,
+    title: 'Webpack',
+    label: 'Tools',
+    icon: Webpack,
+  },
+  webstorm: {
+    switch: true,
+    title: 'WebStorm',
+    label: 'Tools',
+    icon: Webstorm,
+  },
+  intellij: {
+    switch: true,
+    title: 'IntelliJ IDEA',
+    label: 'Tools',
+    icon: Intellij,
+  },
+  figma: {
+    switch: true,
+    title: 'Figma',
+    label: 'Tools',
+    icon: Figma,
+  },
+  photoshop: {
+    switch: true,
+    title: 'Photoshop',
+    label: 'Tools',
+    icon: Photoshop,
+  },
+  illustrator: {
+    switch: true,
+    title: 'Illustrator',
+    label: 'Tools',
+    icon: Illustrator,
+  },
+  xd: {
+    switch: true,
+    title: 'Xd',
+    label: 'Tools',
+    icon: Xd,
+  },
+};
