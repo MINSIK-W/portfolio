ffe915d362a2f5b64ed4ec3647c91322f962ee4e
minsik <minsik90@icloud.com>
2025-03-09 16:24:27 +0900

    line-height 값 수정

--- src/components/MainEffect.tsx
+++ src/components/MainEffect.tsx
@@ -7,7 +7,7 @@ export default function MainEffect() {
     <section className="bg-BgColor2 dark:bg-darkBgColor2 relative m-0 flex h-full w-full items-center justify-center overflow-hidden p-0 select-none">
       <BackgroundTyping />
       <div className="text-textColor dark:text-darkTextColor relative z-10 text-center">
-        <h2 className="font-roboto text-9xl leading-normal -tracking-wide">
+        <h2 className="font-roboto text-9xl leading-tight -tracking-wide">
           {mainEffectTypingData[0].title}
         </h2>
         <MainEffectTyping />
