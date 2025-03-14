c950df5e4e54dc1c86fbae61651d7af2e74cfc97
minsik <minsik90@icloud.com>
2025-03-14 11:37:48 +0900

    경력 컴포넌트 제작

---
+++ src/data/symbol/studyData.ts
@@ -0,0 +1,12 @@
+import { StudySymbolItems } from '@/types/symbol';
+
+export const LearningData: StudySymbolItems = {
+  type: 'study',
+  label: 'Learning',
+  stack: ['react', 'typescript', 'next'],
+};
+export const FavoritesData: StudySymbolItems = {
+  type: 'study',
+  label: 'Favorites',
+  stack: [],
+};
--- src/types/experience.ts
+++ src/types/experience.ts
@@ -1,7 +1,15 @@
-export interface experienceItems {
-  title: string;
-  date: { start: string; end: string }[];
-  job: string;
-  work: string;
-  intro: string[];
+export interface ExperienceDate {
+  start: string;
+  end: string;
+}
+
+export interface ExperienceItem {
+  title: string; // 회사
+  date: ExperienceDate[]; // 시작-종료 날짜
+  job: string; // 직무
+  work: string; // 수행한 작업
+  skill: string[]; // 사용한 기술 ID 목록
+  companyUrl: string; // 기업 홈페이지
+  companyTime: string; // 근무 시간
+  // detailPage?: React.ComponentType<any>; // 세부 페이지
 }
