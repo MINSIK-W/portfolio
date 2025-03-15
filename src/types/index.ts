b3e0356c1ac82bee3af53e3d70a376d1041ce259
minsik <minsik90@icloud.com>
2025-03-15 11:05:54 +0900

    인터페이스 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,12 @@
+import React from 'react';
+
 export interface IndexItem {
   title: string;
 }
 export interface NavigationItems extends IndexItem {
   path: string;
+  icon?: React.ComponentType<any>;
+  subItems?: NavigationItems[];
 }
 export interface MainEffectTypingItem extends IndexItem {
   subTyping: string[];
