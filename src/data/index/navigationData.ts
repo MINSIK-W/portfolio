bf69364604906fb5304616eb3ac3adf2d63ccb2d
minsik <minsik90@icloud.com>
2025-03-15 11:06:50 +0900

    데이터 추가

--- src/data/index/navigationData.ts
+++ src/data/index/navigationData.ts
@@ -1,8 +1,25 @@
 import { NavigationItems } from '@/types/index';
+import Calendar from '@/components/ui/icon/Calendar';
+import Home from '@/components/ui/icon/Home';
+import Moon from '@/components/ui/icon/Moon';
+import Sun from '@/components/ui/icon/Sun';
+import Book from '@/components/ui/icon/Book';
+import Works from '@/components/ui/icon/Works';
+import Components from '@/components/ui/icon/Components';
+import Stack from '@/components/ui/icon/Stack';
 
 export const navigationData: NavigationItems[] = [
   { title: 'Home', path: '/' },
   { title: 'About', path: '/about' },
-  { title: 'Project', path: '/project' },
+  {
+    title: 'Project',
+    path: '/project',
+    subItems: [
+      { icon: Works, title: 'Works', path: '/project/content/works' },
+      { icon: Stack, title: 'Side Project', path: '/project/sideProject' },
+      { icon: Components, title: 'Components', path: '/project/components' },
+      { icon: Book, title: 'Blog', path: '/project/blog' },
+    ],
+  },
   { title: 'Contact', path: '/contact' },
 ];
