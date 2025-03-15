b5a2d325f7fe5ef117e5ebf2d04d22e791e6aa5a
minsik <minsik90@icloud.com>
2025-03-15 11:09:44 +0900

    더미 컴포넌트

---
+++ src/app/project/component/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="bg-bgColor dark:bg-darkBgColor dark:inset-shadow-d inset-shadow-l text-textColor dark:text-darkTextColor">
+      Header
+    </div>
+  );
+}
