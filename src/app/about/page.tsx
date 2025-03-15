cd54c67f278a297b03bf0f6e446342854ae8d515
minsik <minsik90@icloud.com>
2025-03-15 11:07:24 +0900

    Experience 컴포넌트 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,13 @@
 import Intro from '@/app/about/components/Intro';
 import Stack from '@/app/about/components/Stack';
+import Experience from '@/app/about/components/Experience';
 
 export default function AboutPage() {
   return (
     <>
       <Intro />
       <Stack />
-      <Stack />
+      <Experience />
     </>
   );
 }
