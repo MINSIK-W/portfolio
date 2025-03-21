9423cc6840a816a58f76168aa909ad7fad9662e6
minsik <minsik90@icloud.com>
2025-03-22 03:23:50 +0900

    경로지정

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { navigationData } from '@/data/index/navigationData';
+import { isNavItemActive } from '@/utils/navigationUtils';
 
 export default function Navigation() {
   const pathName = usePathname();
@@ -9,7 +10,7 @@ export default function Navigation() {
       <ul className="mr-0.5 flex h-full items-center">
         {navigationData.map((item) => (
           <li
-            className={`${pathName === item.path ? 'bg-hovColor hover:bg-bgColor hover:inset-shadow-l2 dark:bg-darkHovColor dark:hover:bg-darkBgColor dark:hover:inset-shadow-d2' : 'text-textColor dark:text-darkTextColor hover:bg-hovColor dark:hover:bg-darkHovColor'} ml-1 flex cursor-pointer items-center rounded-sm px-2 py-1 duration-300 ease-in`}
+            className={`${isNavItemActive(item, pathName) ? 'bg-hovColor hover:bg-bgColor hover:inset-shadow-l2 dark:bg-darkHovColor dark:hover:bg-darkBgColor dark:hover:inset-shadow-d2' : 'text-textColor dark:text-darkTextColor hover:bg-hovColor dark:hover:bg-darkHovColor'} ml-1 flex cursor-pointer items-center rounded-sm px-2 py-1 duration-300 ease-in`}
             key={item.path}
           >
             <Link href={item.path}>{item.title}</Link>
