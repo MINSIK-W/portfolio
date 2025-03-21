a5d83be93f3a8ec6abecaa013f3b87bfc978082f
minsik <minsik90@icloud.com>
2025-03-22 03:28:09 +0900

    그룹 폴더로 이동

---
+++ src/app/(main)/about/page.tsx
@@ -0,0 +1,13 @@
+import Intro from '@/app/(main)/about/components/Intro';
+import Stack from '@/app/(main)/about/components/Stack';
+import Experience from '@/app/(main)/about/components/Experience';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Intro />
+      <Stack />
+      <Experience />
+    </>
+  );
+}
