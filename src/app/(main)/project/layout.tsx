a2c0190c24a118393b34001b226826c7374a3591
minsik <minsik90@icloud.com>
2025-03-22 03:22:25 +0900

    프로젝트 레이아웃 제작

---
+++ src/app/(main)/project/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from '@/app/(main)/project/component/Header';
+import Navigation from '@/app/(main)/project/component/Navigation';
+
+interface AboutLayoutProps {
+  children: React.ReactNode;
+}
+export default function ProjectLayout({ children }: AboutLayoutProps) {
+  return (
+    <section className="bg-bgColor2 dark:bg-darkBgColor2 relative h-full">
+      <Header />
+
+      <div className="flex h-[calc(100vh-112px)]">
+        <div className="border-bgColor4 dark:border-darkBgColor4 bg-bgColor dark:bg-darkBgColor w-20 border-r">
+          <Navigation />
+        </div>
+        {children}
+      </div>
+    </section>
+  );
+}
