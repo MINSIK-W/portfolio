b8a2e1b9c9b866ff89baffcf895f664309412f2b
minsik <minsik90@icloud.com>
2025-03-22 03:25:10 +0900

    footer 스타일 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import { footerInfoData } from '@/data/index/footerInfoData';
 export default function Footer() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <footer className="bg-bgColor dark:bg-darkBgColor shadow-theme-a dark:shadow-darkTheme-a text-textColor dark:text-darkTextColor z-10 flex flex-row items-center px-3 py-0 text-xs select-none">
+    <footer className="bg-bgColor dark:bg-darkBgColor shadow-theme-a dark:shadow-darkTheme-a text-textColor dark:text-darkTextColor relative z-10 flex h-10 flex-row items-center justify-center px-3 py-0 text-xs select-none">
       {footerInfoData.map((item) => (
         <p key={item.title} className="text-center">
           {item.title}
@@ -15,7 +15,7 @@ export default function Footer() {
       ))}
       <button
         onClick={toggleTheme}
-        className="before:bg-borderColor relative cursor-pointer"
+        className="border-borderColor dark:border-darkBorderColor absolute right-0 h-full cursor-pointer border-l px-4 text-2xl"
         aria-label={theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'}
       >
         <i className={theme === 'dark' ? 'text-Yellow' : 'text-Red'}>
