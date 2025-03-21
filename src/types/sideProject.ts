766e07329dfe35a381eb53a6653c8cad26ac321d
minsik <minsik90@icloud.com>
2025-03-22 03:36:37 +0900

    사이드 프로젝트 인터페이스

---
+++ src/types/sideProject.ts
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export interface SideProjectBase {
+  title: string;
+  detail: string;
+}
+export interface SideProjectToggle {
+  switch: boolean;
+  date: number;
+}
+export interface SideProjectDetail extends SideProjectBase {
+  icon: React.ComponentType<any>;
+}
+export interface SideProjectIntro {
+  details: SideProjectDetail[];
+  description: string[];
+}
+
+export interface SideProjectItem {
+  exposure: boolean;
+  toggle: SideProjectToggle[];
+  title: string;
+  note: string[];
+  intro: SideProjectIntro[];
+  path: string;
+  pathLive: string;
+  technologies: string[];
+  githubUrl: string;
+  features: SideProjectDetail[];
+  challenges: string[];
+  learnings: string[];
+  improvement: SideProjectDetail[];
+}
