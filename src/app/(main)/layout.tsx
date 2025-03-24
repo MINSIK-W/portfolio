b85d66306f2d25db749e9a4990ce15b5573bb0e9
minsik <minsik90@icloud.com>
2025-03-24 17:49:28 +0900

    파일명 변경

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import '@/app/styles/globals.css';
-import { ThemeProvider } from '@/components/Theme';
+import { ThemeProvider } from '@/components/ThemeProvider';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import React from 'react';
--- src/app/(main)/project/content/(sidebar)/layout.tsx
+++ src/app/(main)/project/content/(sidebar)/layout.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-interface AboutLayoutProps {
+interface SideBarLayoutProps {
   children: React.ReactNode;
 }
-export default function SideBarLayout({ children }: AboutLayoutProps) {
+export default function SideBarLayout({ children }: SideBarLayoutProps) {
   return <div className="grid w-full grid-cols-12 overflow-hidden">{children}</div>;
 }
